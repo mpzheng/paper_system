883c833fc83b4acb49a8a722a7dc6d28bcd53c25
ssf-czh <819435330@qq.com>
2020-04-29 18:04:41 +0800

    4.24 无新内容

---
+++ tools/random_pick.py
@@ -0,0 +1,9 @@
+import random
+
+def random_pick(probabilities):
+    x = random.uniform(0, 1)
+    cumulative_probability = 0.0
+    for item_probability in probabilities:
+        cumulative_probability += item_probability
+        if x < cumulative_probability:
+            return probabilities.index(item_probability)
